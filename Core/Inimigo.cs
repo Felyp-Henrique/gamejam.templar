12d16daa5bcdf7632d250997a14566299e983113
Felyp-Henrique <felyphenrique2015@gmail.com>
2022-10-18 20:29:54 -0300

    dev: feito area de colisao de ataque com pausa na perseguicao do inimigo

--- Core/Inimigo.cs
+++ Core/Inimigo.cs
@@ -15,24 +15,38 @@ public class Inimigo : KinematicBody2D
     public NodePath AlcancePath = "./Alcance";
 
     [Export]
+    public NodePath AtaquePath = "./Ataque";
+
+    [Export]
+    public float AtaqueDelay = 1f;
+
+    [Export]
     public NodePath AlvoPath;
 
     private KinematicBody2D _Alvo;
     private AnimatedSprite _Sprite;
     private Area2D _Alcance;
+    private Area2D _Ataque;
+    private float _AtaqueTime;
 
     public override void _Ready()
     {
         _Alcance = GetNode<Area2D>(AlcancePath);
+        _Ataque = GetNode<Area2D>(AtaquePath);
         _Sprite = GetNode<AnimatedSprite>(SpritePath);
-        if (AlvoPath != null) {
+        if (AlvoPath != null)
+        {
             _Alvo = GetNode<KinematicBody2D>(AlvoPath);
         }
     }
 
     public override void _PhysicsProcess(float delta)
     {
-        if (_Alvo != null && _Alcance.OverlapsBody(_Alvo))
+        /**
+         * Movimentação e Perseguição
+         */
+
+        if (_Alvo != null && _Alcance.OverlapsBody(_Alvo) && !_Ataque.OverlapsBody(_Alvo))
         {
             Vector2 direcao = Position.DirectionTo(_Alvo.Position);
 
@@ -43,6 +57,8 @@ public class Inimigo : KinematicBody2D
                 _Sprite.FlipH = direcao.x < 0;
                 _Sprite.Play("walk");
             }
+
+            _AtaqueTime = 0;
         }
         else
         {
@@ -50,6 +66,24 @@ public class Inimigo : KinematicBody2D
             {
                 _Sprite.Play("idle");
             }
+        }
+
+        /**
+         * Ataque
+         */
+
+        if (_Alvo != null && _Ataque.OverlapsBody(_Alvo))
+        {
+            if (_AtaqueTime > AtaqueDelay)
+            {
+                /* programar dano no alvo */
+
+                GD.Print("Recebeu dano o alvo!");
+
+                _AtaqueTime = 0;
+            }
+
+            _AtaqueTime += delta;
         }
     }
 }
